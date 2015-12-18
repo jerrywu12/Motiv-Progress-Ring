7acfb86f51106f0b7734e15d22d9ed9ee761af40
Jerry Wu <jerrywu12@gmail.com>
2015-12-18 17:36:06 -0500

    Initial Commit

---
+++ Motiv Progress RingTests/Motiv_Progress_RingTests.m
@@ -0,0 +1,39 @@
+//
+//  Motiv_Progress_RingTests.m
+//  Motiv Progress RingTests
+//
+//  Created by Jerry Wu on 12/18/15.
+//  Copyright © 2015 Jerry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Motiv_Progress_RingTests : XCTestCase
+
+@end
+
+@implementation Motiv_Progress_RingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
